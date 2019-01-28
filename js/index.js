6a522d6d043515b14d7b9c72267c3ed6ffd7de7a
JS <jsoto3000@outlook.com>
2019-01-28 00:03:01 -0500

    added switch statement with sound

--- js/index.js
+++ js/index.js
@@ -27,19 +27,61 @@ for (var i = 0; i < classButton.length; i++) {
     classButton[i].addEventListener('click', clickButton, false);
 }
 
+// audio = new Audio('./sounds/tom-1.mp3');
+// audio.play();
+// console.log(this);
+// this.style.color = "blue";
+
 */
 
-var classButton = document.querySelectorAll(".drum");
+var classButton = document.querySelectorAll(".drum").length;
 
-var drumSound = function() {
+for (var i = 0; i < classButton; i++) {
 
-audio = new Audio('./sounds/tom-1.mp3');
-audio.play();
-console.log(this);
-this.style.color = "blue";
+  document.querySelectorAll(".drum")[i].addEventListener('click', function () {
 
-};
+  var buttonInnerHTML = this.innerHTML;
 
-for (var i = 0; i < classButton.length; i++) {
-    classButton[i].addEventListener('click', drumSound, false);
+    switch (buttonInnerHTML ) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/crash.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/kick-bass.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/snare.mp3');
+      kick.play();
+      break;
+
+
+    default: console.log(buttonInnerHTML);
+
+      }
+    }
+  );
 }
