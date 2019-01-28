f7b0075db6150e130bdc822a589d5a444220adfb
JS <jsoto3000@outlook.com>
2019-01-28 01:15:02 -0500

    added animation with timeout

--- js/index.js
+++ js/index.js
@@ -40,9 +40,28 @@ for (var i = 0; i < classButton; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener('click', function () {
 
-  var buttonInnerHTML = this.innerHTML;
+    var buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML ) {
+    makeSound(buttonInnerHTML);
+
+    buttonAnimation(buttonInnerHTML);
+
+  });
+
+}
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+
+  switch (key) {
     case "w":
       var tom1 = new Audio("sounds/tom-1.mp3");
       tom1.play();
@@ -64,24 +83,35 @@ for (var i = 0; i < classButton; i++) {
       break;
 
     case "j":
-      var snare = new Audio('sounds/crash.mp3');
+      var snare = new Audio('sounds/snare.mp3');
       snare.play();
       break;
 
     case "k":
-      var crash = new Audio('sounds/kick-bass.mp3');
+      var crash = new Audio('sounds/crash.mp3');
       crash.play();
       break;
 
     case "l":
-      var kick = new Audio('sounds/snare.mp3');
+      var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
 
 
-    default: console.log(buttonInnerHTML);
+    default: console.log(key);
 
-      }
-    }
-  );
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
 }
